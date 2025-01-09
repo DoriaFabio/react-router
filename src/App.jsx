cdd6417666e53d1fa82a0e569845593f6c0165d2
DoriaFabio <doriafabio2001@gmail.com>
2025-01-09 13:57:25 +0000

    do scaffolding

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 // import { useState } from 'react'
 import './App.css'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 function App() {
 
   return (
-    <>
+    <BrowserRouter>
       
-    </>
+    </BrowserRouter>
   )
 }
 
